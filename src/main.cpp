6aa189bec31f60d2bd9e5061ec90feca3f07b45c
Colin <Colin@server>
2013-09-26 21:32:29 -0700

    working on stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <string>
+#include "main.h"
+#include "tokenizer.h"
 
-int main(){
-  std::cout << "test" << std::endl;
+Main::Main(bool toStdout):
+  m_Logger(new Logger(toStdout))
+{
+}
+
+Main::~Main(){
+  delete m_Logger;
+}
+
+void Main::Assert(bool passed, std::string msg){
+  if(!passed){
+    throw std::exception(msg);
+  }
+}
+
+//////////////////////////////////////////
+int main(int argc, char** argv){
+  try{
+      
+    Assert(argc == 2, "Incorrect number of arguments: " + std::string(argc));
+    Tokenizer tokenizer();
+    std::cout << "made tokenizer" << std::endl;
+
+  }catch(std::exception& e){
+    std::cout << "[ERROR] " << e.what();
+    exit(0);
+  }
 }
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -2,6 +2,9 @@
 #define TOKENIZER_H
 
 class Tokenizer{
+  // break up each token by whitespace
+  // interpret the token's type (symbol, keyword, identifier)
+  // print each token to a file, wrapped in tags
 };
 
 #endif
