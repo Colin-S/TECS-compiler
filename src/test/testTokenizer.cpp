05237f689311c72d1c2be4a8af7e78510a2e64aa
Colin <Colin@server>
2014-02-14 20:39:45 -0800

    updated tokenizer test

--- src/test/testTokenizer.cpp
+++ src/test/testTokenizer.cpp
@@ -6,7 +6,7 @@
 #include "../tokenizer.h"
 using namespace std;
 
-SCENARIO("The tokenizer can tokenize jack files", "[tokenizer]") {
+SCENARIO("The tokenizer can tokenize ArrayTest jack files", "[tokenizer]") {
 
   GIVEN("A tokenizer") {
     shared_ptr<Tokenizer> pTokenizer = Tokenizer::buildTokenizer();
@@ -39,5 +39,39 @@ SCENARIO("The tokenizer can tokenize jack files", "[tokenizer]") {
       ifstream deletedFile(tokenFileName.c_str());
       REQUIRE(deletedFile.fail());
     }
-  }
+}
+
+SCENARIO("The tokenizer can tokenize SquareGame jack files", "[tokenizer]") {
+
+  GIVEN("A tokenizer") {
+    shared_ptr<Tokenizer> pTokenizer = Tokenizer::buildTokenizer();
+    REQUIRE(pTokenizer);
+
+    WHEN("Main.jack is tokenized") {
+      string jackfileName("ArrayTest/Main.jack");
+      string tokenFileName = pTokenizer->tokenize(jackfileName);
+
+      REQUIRE(tokenFileName == "MainT.xml");
+
+      THEN("Main.jack is tokenized correctly") {
+        ifstream tokenFile(tokenFileName.c_str());
+        REQUIRE(!tokenFile.fail());
+        ifstream testFile("ArrayTest/MainT.xml");
+        REQUIRE(!testFile.fail());
+
+        string tokenFileLine;
+        string testFileLine;
+
+        while(getline(tokenFile, tokenFileLine)) {
+          getline(testFile, testFileLine);
+          REQUIRE(testFileLine == tokenFileLine);
+        }
+        REQUIRE(getline(testFile, testFileLine).eof());
+      }
+
+      // Remove the created file
+      remove(tokenFileName.c_str());
+      ifstream deletedFile(tokenFileName.c_str());
+      REQUIRE(deletedFile.fail());
+    }
 }
